30fd9504b50668e9ea566f1b4b1df9d27eaf831e
lht <htle2101@gmail.com>
2014-11-05 17:40:22 +0800

    test

--- app/helpers/new_gem_helper.rb
+++ app/helpers/new_gem_helper.rb
@@ -1,5 +1,5 @@
-class NewGemHelper
-  def text
-    aaa
+module NewGemHelper
+  def new_gem_product(text)
+    render :partial => 'new_gem/products/productll', :locals => {:text => text}
   end
 end
